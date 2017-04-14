6126151b511234166763475025cea6d62a6f9b31
zifanYao <zuoduzuodu24@gmail.com>
2017-04-14 13:31:03 +0200

    essai servlet

---
+++ src/main/java/web/Servlet1.java
@@ -0,0 +1,21 @@
+package web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by zifanYao on 14/04/2017.
+ */
+public class Servlet1 extends javax.servlet.http.HttpServlet {
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+}
